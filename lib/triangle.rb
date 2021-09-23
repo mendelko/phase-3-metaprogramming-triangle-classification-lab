e3f98e9fbde2b6329589db9687f3f3673870f0a3
Mendel <mkonikov123@gmail.com>
2021-09-23 16:23:21 -0400

    commit tree 15f878bcd58d7b4094a3405571c7cb810e2138f4

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
-  # write code here
+
+  def initialize(a, b, c)
+    if a + b <= c || a + c <= b || b + c <= a
+      raise TriangleError
+    elsif a <= 0 || b <= 0 || c <= 0
+      raise TriangleError
+    end
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if @a == @b && @a == @c
+      return :equilateral
+    elsif @a == @b || @a == @c || @b == @c
+      return :isosceles
+    elsif @a != @b && @a != @c && @b != @c
+      return :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    "Invalid triangle"
+  end
 end
